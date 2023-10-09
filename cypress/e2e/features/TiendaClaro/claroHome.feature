9fc4d3d9118c1a5ce7d438d63ae5965d75cf9344
AnnieDroide <anitamercadojuncos@gmail.com>
2023-10-09 10:27:40 -0300

    Test Tienda Claro validar precios de menor a mayor

--- cypress/e2e/features/TiendaClaro/claroHome.feature
+++ cypress/e2e/features/TiendaClaro/claroHome.feature
@@ -5,3 +5,13 @@ Feature: Test de la página de Tienda Claro
         Given que un usuario está en la página de "Claro"
         When posiciona el mouse sobre "Celulares"
         Then se visualizan las opciones "Samsung, Motorola, Celulares Liberados"
+
+    Scenario: Verificar orden de los elementos en PLP
+        Given que un usuario está en la página de "Claro"
+        When se selecciona la opción "Ver catálogo completo"
+        And se cambia filtro a "Menor precio"
+        Then los precios están ordenados de menor a mayor
+
+       
+
+    
--- cypress/pages/clarohome.js
+++ cypress/pages/clarohome.js
@@ -4,7 +4,21 @@ class ClaroHeader {
   }
 
   getHeaderSuboptionMenu(){
-    return cy.get('[data-test="box-container"]')
+    return cy.get('[data-test="box-container"]');
+  }
+
+  getCatalogFilter(){
+    return cy.get('[class="MuiOutlinedInput-root MuiInputBase-root MuiInputBase-colorPrimary MuiSelect-root css-oixtcz"]');
+  }
+
+  getCatalogFilterSubOption(){
+    return cy.get('[role="listbox"] li');
+  }
+  getLoadingIcon(){
+    return cy.get('[data-test="loading"]');
+  }
+  getOfferPrices(){
+    return cy.get('[data-test="price"]');
   }
 }
 
