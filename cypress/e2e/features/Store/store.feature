d71dfb947e14441742829e4a954f0efa29546ac3
AnnieDroide <anitamercadojuncos@gmail.com>
2023-10-11 18:13:11 -0300

    Se agrega test Store y Background

--- cypress/e2e/features/Store/store.feature
+++ cypress/e2e/features/Store/store.feature
@@ -1,8 +1,17 @@
 Feature: Test de la página de Store Automation
+Background: Entrar a Store & Login
+        Given que un usuario está en la página de "Store"
+        When el usuario presiona "Login or register" en el menu principal
+        And completa usuario y password "1"
+        Then se verifica que el titulo principal es "My Account"
 
     Scenario:Realizar Loguin
-        Given que un usuario está en la página de "Store"
-        When cuando el usuario presiona "Login or register" en el menu principal
-        And completa usuario y password "1"
-        Then se verifica que el titulo principal en "My Account"
-        
+        When el usuario realiza hover "Welcome back"
+        And clickea la opción "Logoff"
+        Then se verifica que el titulo principal es "Account Logout"
+
+
+    Scenario:Realizar Loguin & logout
+       
+        When el usuario realiza logout
+        Then se verifica que el titulo principal es "Account Logout"
--- cypress/e2e/step_definitions/store.js
+++ cypress/e2e/step_definitions/store.js
@@ -4,22 +4,39 @@ const storeLogin = require ("../../pages/storeLogin");
 const storeAccount = require ("../../pages/storeAccount");
 
 
-When (`cuando el usuario presiona {string} en el menu principal`, (opcion) => {
+When (`el usuario presiona {string} en el menu principal`, (opcion) => {
     store.getHeaderLinks().contains(opcion).click();
 
+});
+
+When (`el usuario realiza hover {string}`,(opcion) => {
+    store.getHeaderLinks().contains(opcion).trigger("mouseover");
+});
+
+When (`el usuario realiza logout`, ()=> {
+    store.getHeaderLinks().contains("Welcome back").trigger("mouseover");
+    cy.contains("Logoff", {matchCase: false}).click();
 });
 
 When (`completa usuario y password {string}`, (num) => {
     const userData = Cypress.env().users[num-1];
     cy.log(userData);
-    cy.login(userData.user, userData.pass);
+
+    //Login empleando función del mismo PageObject (se crea un PO específico de login)
+    storeLogin.login(userData.user, userData.pass);
+
     
+    //Login agregando un comando en el support/commands.js
+    //cy.login(userData.user, userData.pass);
+
+    //Login usando cada método del PageObject
+
    /* storeLogin.getLoginInput().type(userData.user);
     storeLogin.getPassInput().type(userData.pass);
     storeLogin.getLoginBtn().click();*/
 
 });
 
-Then (`se verifica que el titulo principal en {string}`, (title) => {
+Then (`se verifica que el titulo principal es {string}`, (title) => {
 storeAccount.getMainTitle().should("contain.text", title);
 });
